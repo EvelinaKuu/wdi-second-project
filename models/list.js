7f44ea20d352e910ff0f25fbc3a89ae8b13455be
EvelinaKuu <evelina.kuusinen@gmail.com>
2018-01-15 09:52:21 +0000

    list model ready

---
+++ models/list.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+const productSchema = new mongoose.Schema({
+  name: { type: String, required: true},
+  category: { type: String, required: true}
+});
+
+const listSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  createdBy: { type: mongoose.Schema.ObjectId, ref: 'User', required: true },
+  products: [ productSchema]
+
+});
+
+module.exports = mongoose.model('List', listSchema);
