6e92adc04b419f6ba0259d730661d77159e695e8
EvelinaKuu <evelina.kuusinen@gmail.com>
2018-01-15 10:33:12 +0000

    user model and registration controller updated

--- config/routes.js
+++ config/routes.js
@@ -1,5 +1,11 @@
 const router = require('express').Router();
 
+const registrations = require('../controllers/registrations');
+
 router.get('/', (req, res) => res.render('statics/index'));
+
+router.route('/register')
+  .get(registrations.new)
+  .post(registrations.create);
 
 module.exports = router;
