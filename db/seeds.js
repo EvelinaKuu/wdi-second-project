f2140cab8b26a951044bd197506d66666812edf4
EvelinaKuu <evelina.kuusinen@gmail.com>
2018-01-16 13:41:55 +0000

    added the create List and create User in seeds file

--- db/seeds.js
+++ db/seeds.js
@@ -6,5 +6,29 @@ mongoose.connect(dbURI);
 
 // also const List
 const User = require('../models/user');
+const List = require('../models/list');
 
 User.collection.drop();
+List.collection.drop();
+
+User
+  .create([{
+    username: 'p',
+    email: 'p@p',
+    password: 'p',
+    passwordConfirmation: 'p'
+  }])
+  .then((users) => {
+    console.log(`${users.length} users created`);
+
+    return List
+      .create([{
+        name: 'Things I need to get',
+        createdBy: users[0],
+        products: 'mascara'
+
+      }]);
+  })
+  .then((lists) => console.log(`${lists.length} lists created`))
+  .catch((err) => console.log(err))
+  .finally(() => mongoose.connection.close());
